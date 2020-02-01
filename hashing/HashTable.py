89e77182f6084e8f8edbf0fa833c2685c9758570
David Lorente <david.l.castilla@gmail.com>
2020-02-01 18:20:44 -0500

    add getr setr to hashT

--- hashing/HashTable.py
+++ hashing/HashTable.py
@@ -1,40 +1,44 @@
 class HashTable:
-	def __init__(self):
-		self.count = 0
-		self.space = [None for i in range(255)]
+    def __init__(self):
+        self.count = 0
+        self.space = [None for i in range(255)]
 
-	def hash(self, item):
-		count = 1
-		result = 0
-		for char in item:
-			result += ord(char) * count
-		return result % len(self.space)
+    def hash(self, item):
+        count = 1
+        result = 0
+        for char in item:
+            result += ord(char) * count
+        return result % len(self.space)
 
-	def append(self, item, value):
-		hashvalue = self.hash(item)
-		if self.space[hashvalue] is None:
-			self.space[hashvalue] = [item, value]
-		else:
-			foundSpace = False
-			count = 1
-			while not foundSpace:
-				if self.space[hashvalue + count] is None:
-					foundSpace = True
-					self.space[hashvalue + count] = [item, value]
+    def append(self, item, value):
+        hashvalue = self.hash(item)
+        if self.space[hashvalue] is None:
+            self.space[hashvalue] = [item, value]
+        else:
+            foundSpace = False
+            count = 1
+            while not foundSpace:
+                if self.space[hashvalue + count] is None:
+                    foundSpace = True
+                    self.space[hashvalue + count] = [item, value]
 
-	def get(self, item):
-		hashvalue = self.hash(item)
-		if self.space[hashvalue] and self.space[hashvalue][0] == item:
-			return self.space[hashvalue][1]
-		else:
-			foundSpace = False
-			count = 1
-			while not foundSpace:
-				if self.space[hashvalue + count] and self.space[hashvalue + count][1] == item:
-					foundSpace = True
-					return self.space[hashvalue + count][1]
+    def get(self, item):
+        hashvalue = self.hash(item)
+        if self.space[hashvalue] and self.space[hashvalue][0] == item:
+            return self.space[hashvalue][1]
+        else:
+            foundSpace = False
+            count = 1
+            while not foundSpace:
+                if self.space[hashvalue + count] and self.space[hashvalue + count][1] == item:
+                    foundSpace = True
+                    return self.space[hashvalue + count][1]
 
+    def __setitem__(self, key, value): 
+        self.append(key, value) 
 
+    def __getitem__(self, key): 
+        return self.get(key) 
 
 table = HashTable()
 
@@ -47,3 +51,8 @@ table.append("lime", 3)
 
 print(table.get("avocado"))
 print(table.get("lime"))
+
+
+table["pear"] = 5
+
+print(table["pear"])
